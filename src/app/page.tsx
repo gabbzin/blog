c28b7de723cd144299d9e182aacb7b32c542fec0
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-27 16:55:57 -0300

    Loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import Image from "next/image";
+import { postRepository } from "@/repositories/post";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
-      <SpinLoader />
+      <header>
+        <h1>Aqui é o HEADER</h1>
+      </header>
+      <main>
+        <Suspense fallback={<SpinLoader />}>
+          <PostsList />
+        </Suspense>
+      </main>
+      <footer>
+        <h1>Aqui é o FOOTER</h1>
+      </footer>
     </div>
   );
 }
--- src/components/SpinLoader/index.tsx
+++ src/components/SpinLoader/index.tsx
@@ -1,12 +1,12 @@
 import clsx from "clsx";
 
 type SpinLoaderProps = {
-  containerClasses?: string;
+  className?: string;
 };
 
-export function SpinLoader({ containerClasses }: SpinLoaderProps) {
+export function SpinLoader({ className }: SpinLoaderProps) {
   return (
-    <div className={clsx("flex", "items-center", "justify-center", "h-screen")}>
+    <div className={clsx("flex", "items-center", "justify-center", className)}>
       <div className="w-10 h-10 border-4 border-t-transparent rounded-full border-slate-900 animate-spin"></div>
     </div>
   );
