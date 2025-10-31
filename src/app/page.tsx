2e50a20e5f33daa5a257028e81c3abbcd2c240d5
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-31 13:11:05 -0300

    Mexendo no layout, aprendendo metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
 import { PostFeatured } from "@/components/PostFeatured";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
@@ -7,19 +5,14 @@ import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <Container>
-      <Header />
+    <>
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
 
-      <PostFeatured />
-
-      <main>
-        <Suspense fallback={<SpinLoader />}>
-          <PostsList />
-        </Suspense>
-      </main>
-      <footer>
-        <h2>Aqui é o FOOTER</h2>
-      </footer>
-    </Container>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+    </>
   );
 }
