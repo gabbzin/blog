e771a721ab3670f42d6bb70b9135e04d7ccf4c7c
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-31 13:48:05 -0300

    Personalizando a Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
 
-export function Footer(){
-  return <footer className="pb-20">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quia reprehenderit laudantium. Quis aperiam eveniet fugiat ipsum, iusto non debitis nisi laboriosam impedit, deserunt asperiores magnam minus delectus quae dolores.
-  </footer>
+export function Footer() {
+  return (
+    <footer className="pb-20 text-center">
+      <p>
+        Copyright &copy; {new Date().getFullYear()} -{" "}
+        <Link href={"/"}>The Blog</Link>
+      </p>
+    </footer>
+  );
 }
