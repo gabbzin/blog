63f5dbb8a3c0b23e71d411d42038ad1167c10516
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-10-31 14:47:43 -0300

    feat: adiciona função para gerar metadados de postagens com tratamento de erro

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,23 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = await findPostBySlugCached(slug).catch(() => undefined);
+
+  return {
+    title: post?.title || "Post não encontrado",
+    description: post?.excerpt || "Descrição não disponível",
+  };
+}
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
