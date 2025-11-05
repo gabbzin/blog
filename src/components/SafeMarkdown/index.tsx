18cddf0d662408076b996ce3863f8722e38a7420
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-11-05 11:39:46 -0300

    Estilização personalizada para tabelas de markdown

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import clsx from "clsx";
 import ReactMarkdown from "react-markdown";
 import rehypeSanitize from "rehype-sanitize";
@@ -12,16 +12,34 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
     <div
       className={clsx(
-        "prose prose-slate max-w-none",
+        "prose prose-slate",
         "w-full max-w-none",
-        "prose-a:text-blue-500 hover:prose-a:underline hover:prose-a:text-blue-700",
+        "overflow-hidden",
+        "prose-a:transition",
+        "prose-a:no-underline",
+        "prose-a:text-blue-500",
+        "prose-a:hover:text-blue-700",
+        "prose-a:hover:underline",
         "prose-img:mx-auto",
-        "md:prose-lg",
+        "lg:prose-lg"
       )}
     >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) {
+              return "";
+            }
+
+            return (
+              <div className="overflow-x-auto">
+                <table className="w-full min-w-600px" {...props} />
+              </div>
+            );
+          },
+        }}
       >
         {markdown}
       </ReactMarkdown>
